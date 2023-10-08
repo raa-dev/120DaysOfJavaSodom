7c7658f56d8730fddcc11bc5c0396d6ecfa47cd8
Rodrigo Ramos Xochiteotzin <rodrx20@gmail.com>
2023-10-07 20:26:09 -0600

    gradle and testing setup updated | exercise 2 done

--- chapter2/exercise1/src/main/java/BasicArithmetic.java
+++ chapter2/exercise1/src/main/java/BasicArithmetic.java
@@ -32,4 +32,14 @@ public class BasicArithmetic {
 
         return result;
     }
+
+    /*Write method int calc(int, int) that multiplies two variables, m and n of type int,
+    then divides the product by two, and outputs the remainder with respect to division by 7.*/
+
+    public int calc(int m, int n) {
+
+        return ((m * n)/2) % 7;
+    }
+
+
 }
--- chapter2/exercise1/src/test/java/BasicArithmeticTest.java
+++ chapter2/exercise1/src/test/java/BasicArithmeticTest.java
@@ -1,5 +1,7 @@
-import org.junit.jupiter.api.Test;
+
+
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 
 public class BasicArithmeticTest {
@@ -8,7 +10,13 @@ public class BasicArithmeticTest {
     public void testCalcSumAndCountAllNumbersDivBy_2_Or_7(){
         BasicArithmetic exercise = new BasicArithmetic();
         int[] result = exercise.calcSumAndCountAllNumbersDivBy_2_Or_7(15);
+        int calcResult = exercise.calc(6,7);
+        int calcResult2 = exercise.calc(3,4);
+        int calcResult3 = exercise.calc(5,5);
         Assertions.assertEquals(8,result[0]);
         Assertions.assertEquals(63,result[1]);
+        Assertions.assertEquals(0,calcResult);
+        Assertions.assertEquals(6,calcResult2);
+        Assertions.assertEquals(5,calcResult3);
     }
 }
